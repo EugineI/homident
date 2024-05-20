4e7989c5337708a557d8a870fd3855e1923001f1
EugineI <eugineindimuli@gmail.com>
2024-05-20 13:58:30 +0300

    config2

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Use the Python installed in the Vercel environment
+# Create a virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
 # Upgrade pip to the latest version
-pip install --upgrade pip
+venv/bin/pip install --upgrade pip
 
 # Install dependencies
-pip install -r requirements.txt
+venv/bin/pip install -r requirements.txt
 
 # Django specific commands
-python manage.py collectstatic --noinput
-python manage.py migrate
+venv/bin/python manage.py collectstatic --noinput
+venv/bin/python manage.py migrate
