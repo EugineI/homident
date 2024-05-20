3fbacd1515cc71978ea90c687e49d4aa91b17aa3
EugineI <eugineindimuli@gmail.com>
2024-05-20 14:54:30 +0300

    b

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,22 @@
-#!/bin/bash#!/bin/bash
+#!/bin/bash
 
-# Ensure pip is installed and upgrade it
+# Create a virtual environment if it doesn't exist
+if [ ! -d "home" ]; then
+    python -m venv home
+fi
+
+# Activate the virtual environment
+source home/bin/activate
+
+# Ensure pip is available and upgrade it
 pip install --upgrade pip
 
-# Install dependencies globally
+# Install dependencies
 pip install -r requirements.txt
 
 # Django specific commands
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Deactivate the virtual environment
+deactivate
